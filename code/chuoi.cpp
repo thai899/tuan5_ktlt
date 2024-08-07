a4741af7f3f81e7f7285b2913c86bc4df33ec866
Thai <trinhhoangthai2005@gmail.com>
2024-08-07 16:02:05 +0700

    de quy

---
+++ code/chuoi.cpp
@@ -0,0 +1,38 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <limits.h>
+#include<string.h>
+void nhapChuoi(char* str, int size) {
+    printf("Nhap chuoi: ");
+    fgets(str, size, stdin);
+    str[strcspn(str, "\n")] = '\0';
+}
+
+
+void xuatChuoi(const char* str) {
+    printf("%s\n", str);
+}
+
+
+int kiemTraDoiXung(const char* str) {
+    int len = strlen(str);
+    for (int i = 0; i < len / 2; i++) {
+        if (str[i] != str[len - 1 - i]) {
+            return 0;
+        }
+    }
+    return 1;
+}
+int main()
+{
+    char str[100];
+    nhapChuoi(str, sizeof(str));
+    if (kiemTraDoiXung(str)) {
+        printf("Chuoi doi xung.\n");
+    }
+    else {
+        printf("Chuoi khong doi xung.\n");
+    }
+    return 0;
+}
---
+++ code/dequy.cpp
@@ -0,0 +1,19 @@
+﻿#include <stdio.h>
+
+
+void thapHaNoi(int n, char tu, char den, char trungGian) {
+    if (n == 1) {
+        printf("Chuyen dia 1 tu %c sang %c\n", tu, den);
+        return;
+    }
+    thapHaNoi(n - 1, tu, trungGian, den);
+    printf("Chuyen dia %d tu %c sang %c\n", n, tu, den);
+    thapHaNoi(n - 1, trungGian, den, tu);
+}
+int main() {
+    int n; 
+    printf("Nhap so luong dia: ");
+    scanf_s("%d", &n);
+    thapHaNoi(n, 'A', 'C', 'B'); 
+    return 0;
+}
